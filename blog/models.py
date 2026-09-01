d49df5cab5d5c606dbcccf8537d0ff88e45139ff
Eng . Seif Eldeen <seifedeen112006@gmail.com>
2026-09-02 01:03:08 +0300

    update Task-day-3

--- blog/models.py
+++ blog/models.py
@@ -36,6 +36,7 @@ class Blog(models.Model):
     price = models.FloatField()
     description = models.TextField()
     image = models.ImageField(upload_to="blog_images/", null=True , blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='blogs', null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs', null=True, blank=True)
 
